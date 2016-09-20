db30a071b48c3db6bd1534f091f838b2512f82e7
Jay Gradon <jgra718@aucklanduni.ac.nz>
2016-09-20 15:22:56 +1200

    Initial file upload

---
+++ tinyDOS.py
@@ -0,0 +1,50 @@
+import Drive
+import os
+
+class tinyDOS:
+
+	def __init__(self, is):
+		pass
+
+	def format(self, name):
+		pass
+
+	def reconnect(self, name):
+		pass
+
+	def ls(self, path):
+		pass
+		
+	def mkfile(self, path):
+		pass
+		
+	def mkdir(self, path):
+		pass
+		
+	def append(self, path, data):
+		pass
+		
+	def print(self, path):
+		pass
+		
+	def delfile(self, path):
+		pass
+		
+	def deldir(self, path):
+		pass
+		
+	def quit(self):
+		pass
+		
+class Volume:
+
+	EMPTY_BITMAP = '--------------------------------------------------------------------------------------------------------------------------------'
+	
+	def __init__(self):
+		pass
+
+	def format(self, name):
+		pass
+	
+if __name__ == '__main__':
+	print Volume.EMPTY_BITMAP
